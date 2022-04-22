207083761d6e24808e81c77d5fd57f0fa4d1d57a
Thiago Pereira <thiagopereiramail@gmail.com>
2022-04-22 07:30:44 -0300

    Start the repository

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0c5323a16f4d965538b033501f724cee'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f03e84cb-0663-49c1-8165-af45a3eb7e83'
+};
